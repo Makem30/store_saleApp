979550ba8e2766d5714bed1eba72b10f642950c9
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-12-15 20:37:35 +0100

    Update StoreSale.py

--- StoreSale.py
+++ StoreSale.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import MinMaxScaler
-import joblib
+# from sklearn.ensemble import RandomForestRegressor
+# from sklearn.preprocessing import MinMaxScaler
+# import joblib
 
 # Charger les données et le modèle
 @st.cache
