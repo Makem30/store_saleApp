9a072206b9f48fb94dad39717abc3506ec294d7a
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-12-20 14:57:18 +0100

    Update StoreSale.py

--- StoreSale.py
+++ StoreSale.py
@@ -30,44 +30,14 @@ st.sidebar.title("Filtres")
 # selected_month = st.sidebar.selectbox("Sélectionner le mois", train['date'].dt.month.unique())
 
 # Filtrer les données en fonction des sélections
-import streamlit as st
-import pandas as pd 
-import altair as alt
-import numpy as np
-chart = alt.Chart(filtered_data).mark_bar().encode(
-       alt.X("sales:Q", bin=True),
-       alt.Y("count()")
-   ).properties(
-       title=f"Distribution des ventes pour {selected_month}/{selected_year}"
-   )
-st.altair_chart(chart, use_container_width=True)
-
-# import zipfile
-# with zipfile.ZipFile("train.zip", "r") as zip_ref:
-#     # zip_ref.extractall("train.zip")
-#     train = pd.read_csv("train.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Create two columns
-# col1, col2 = st.columns([0.5,0.4],gap="large")
-
-# with col1:
-
-  #Content for the first column
-# with col2:
-
-# with st.sidebar:
-#   st.title('DASHBOARD')
-#   st.sidebar.title("Comparateur de données")
+# import streamlit as st
+# import pandas as pd 
+# import altair as alt
+# import numpy as np
+# chart = alt.Chart(filtered_data).mark_bar().encode(
+#        alt.X("sales:Q", bin=True),
+#        alt.Y("count()")
+#    ).properties(
+#        title=f"Distribution des ventes pour {selected_month}/{selected_year}"
+#    )
+# st.altair_chart(chart, use_container_width=True)
