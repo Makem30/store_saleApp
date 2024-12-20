6993daaaa0de7a567aece7b20113d17de9112148
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-12-20 15:19:39 +0100

    Update StoreSale.py

--- StoreSale.py
+++ StoreSale.py
@@ -2,7 +2,7 @@
 # import pandas as pd 
 # import altair as alt
 # import numpy as np
-# import plotly.express as px
+import plotly.express as px
 
 # import matplotlib.pyplot as plt
 # import seaborn as sns
