5fceffdc6b0b2912a029c1e6311d019d23467dfe
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-12-20 14:52:02 +0100

    Update StoreSale.py

--- StoreSale.py
+++ StoreSale.py
@@ -7,7 +7,7 @@ import numpy as np
 
 #Charger les Données :
 holidays_events = pd.read_csv('holidays_events.csv', delimiter=",")
-# train = pd.read_csv('train.csv', delimiter=",")
+train = pd.read_csv('train.csv', delimiter=",")
 test = pd.read_csv('test.csv', delimiter=",")
 stores = pd.read_csv('stores.csv', delimiter=",")
 oil = pd.read_csv('oil.csv', delimiter=",")
@@ -15,9 +15,14 @@ sample_submission = pd.read_csv('sample_submission.csv', delimiter=",")
 transactions = pd.read_csv('transactions.csv', delimiter=",")
 
 
-# Charger les données
-train = pd.read_csv('train.csv', delimiter=",") 
+
 train['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+oil['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+sample_submission['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+transactions['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+test['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+holidays_events['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
+stores['date'] = pd.to_datetime(train['date'])  # Convertir la colonne 'date' en datetime
 
 # Créer la sidebar
 st.sidebar.title("Filtres")
